9deaca5c63a3f26e77f96219c057b39c97b56ee2
debjyotiC <djbabai.debjyoti@gmail.com>
2021-11-20 20:16:26 +0530

    deleted old files

--- tflite_converter.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-converter = tf.lite.TFLiteConverter.from_saved_model('model/fft-clf')
-
-tflite_model = converter.convert()
-
-open("model/tflite_model/converted_model.tflite", "wb").write(tflite_model)
