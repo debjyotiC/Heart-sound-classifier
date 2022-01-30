6d146a7bae393865caf2f918a202c9d2f4511454
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2022-01-30 14:02:42 +0530

    working classifier v0.3 and saved model added

---
+++ dnn_heart_audio_v0.3.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+tf.config.list_physical_devices('GPU')
+tf.test.is_gpu_available()
+
+data = np.load("data/mfcc-flattened.npz", allow_pickle=True)
+x_data, y_data = data['out_x'], data['out_y']
+
+classes_values = ["murmur", "normal"]
+classes = len(classes_values)
+
+y_data = tf.keras.utils.to_categorical(y_data - 1, classes)
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=1, shuffle=True)
+input_length = x_train[0].shape[0]
+print(input_length)
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+validation_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Reshape((int(input_length / 13), 13), input_shape=(input_length,)),
+
+    tf.keras.layers.Conv1D(8, kernel_size=3, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Conv1D(16, kernel_size=3, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Flatten(),
+
+    # Dense layer
+    tf.keras.layers.Dense(classes, activation='softmax', name='y_pred')
+])
+
+model.summary()
+model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.005, beta_1=0.9, beta_2=0.999),
+              metrics=['acc'])
+
+# this controls the batch size
+BATCH_SIZE = 32
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
+validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
+
+history = model.fit(train_dataset, epochs=150, validation_data=validation_dataset)
+
+model.save("saved_model/mfcc_flattened")
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+print(f"Training Accuracy: {round(np.average(acc), 3)}")
+print(f"Validation Accuracy: {round(np.average(val_acc), 3)}")
+
+epochs = range(1, len(acc) + 1)
+fig, axs = plt.subplots(2, 1)
+
+# plot loss
+axs[0].plot(epochs, loss, '-', label='Training loss')
+axs[0].plot(epochs, val_loss, 'b', label='Validation loss')
+axs[0].set_xlabel('Epoch')
+axs[0].set_ylabel('Loss')
+axs[0].grid(True)
+axs[0].legend(loc='best')
+# plot accuracy
+axs[1].plot(epochs, acc, '-', label='Training acc')
+axs[1].plot(epochs, val_acc, 'b', label='Validation acc')
+axs[1].set_xlabel('Epoch')
+axs[1].set_ylabel('Accuracy')
+axs[1].grid(True)
+axs[1].legend(loc='best')
+plt.show()
