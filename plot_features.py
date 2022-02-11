c9df7f87797160e90fe8d4952c857f8cfad1f70e
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-02-11 14:38:48 +0530

    feature plotter added

--- plot_features.py
+++ plot_features.py
@@ -8,21 +8,23 @@ features_mfe = np.load("data/mfe.npz", allow_pickle=True)
 mfcc_data, mfcc_labels = features_mfcc['out_x'], features_mfcc['out_y']
 mfe_data, mfe_labels = features_mfe['out_x'], features_mfe['out_y']
 
-pos = 0
+pos = 10
+num_frames = 148
+num_ceps = 26
 
-mfcc_data_0 = mfcc_data[pos].transpose()
-mfe_data_0 = mfe_data[pos].transpose()
+mfcc_data = mfcc_data[pos].reshape(num_frames, num_ceps).transpose()
+mfe_data = mfe_data[pos].reshape(num_frames, num_ceps).transpose()
 
 
 fig, ax = plt.subplots(2, 1)
 # MFCC plot
-# ax[0].set_axis_off()
-ax[0].imshow(mfcc_data_0, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+ax[0].set_axis_off()
+ax[0].imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
 ax[0].set_title('MFCC plot')
 
 # MFE plot
-# ax[1].set_axis_off()
-ax[1].imshow(mfe_data_0, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+ax[1].set_axis_off()
+ax[1].imshow(mfe_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
 ax[1].set_title('MFE plot')
 plt.savefig("test.png")
 plt.show()
