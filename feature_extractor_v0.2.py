cb4b56a42794d512943e68e76dffbf38e73bef15
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-02-17 11:24:37 +0530

    feature and confusion image updated

--- feature_extractor_v0.2.py
+++ feature_extractor_v0.2.py
@@ -7,7 +7,7 @@ import numpy as np
 np.set_printoptions(suppress=True)
 dataset_path = '/Users/deb/Documents/heart-data'
 SAVE = True
-sample_rate = 16000
+
 
 frame_length = 0.5
 frame_stride = 0.01
--- read_files_test.py
+++ read_files_test.py
@@ -1,34 +1,41 @@
-import python_speech_features as psf
+import speechpy as sp
 import librosa
 import numpy as np
 import tensorflow as tf
 
 np.set_printoptions(suppress=True)
 
-sample_rate = 16000
-num_mfcc = 26
+frame_length = 0.5
+frame_stride = 0.01
+fft_size = 256
 
-path_murmur = "C:/Users/Deb/Documents/heart-data/murmur/murmur__122_1306325762831_C.wav"
-path_normal = "C:/Users/Deb/Documents/heart-data/normal/normal__103_1305031931979_B.wav"
-path_unlabeled_test = "C:/Users/Deb/Documents/heart-data/exhaled/extrahls__201101070953.wav"
+num_filter = 26
+num_ceps = 26
+
+pre_cof = 0.97
+pre_shift = 1
+
+time = 2.0
+num_frames = 150
+
+path_murmur = "/Users/deb/Documents/heart-data/murmur/murmur__122_1306325762831_C.wav"
 
 
-def calc_mfcc(file_path, s_rate):
-    signal, fs = librosa.load(file_path, sr=s_rate)
-    signal = signal[0:int(1.5 * sample_rate)]  # keep first 3 sec of the audio data
-    mfccs = psf.base.mfcc(signal, samplerate=fs, winlen=0.256, winstep=0.050, numcep=num_mfcc, nfilt=26,
-                          nfft=4096, preemph=0.0, ceplifter=0, appendEnergy=False, winfunc=np.hanning)
-    return mfccs.transpose()
+def calc_MFCC(path):
+    signal, fs = librosa.load(path, sr=None)
+    signal = signal[0: int(time * fs)]
+    signal_pre_emphasized = sp.processing.preemphasis(signal, cof=pre_cof, shift=pre_shift)
+    mfccs = sp.feature.mfcc(signal_pre_emphasized, sampling_frequency=fs, frame_length=frame_length,
+                            frame_stride=frame_stride, num_cepstral=num_ceps, num_filters=num_filter,
+                            fft_length=fft_size)
+    return mfccs.flatten()
 
 
-mfcc_got = calc_mfcc(file_path=path_murmur, s_rate=sample_rate)
-mfcc_got = mfcc_got.reshape(1, mfcc_got.shape[0], mfcc_got.shape[1], 1)
+mfcc_got = calc_MFCC(path=path_murmur)
+print(mfcc_got.shape)
 
 loaded_model = tf.keras.models.load_model("saved_model/mfcc")
 classes = loaded_model.predict(mfcc_got)
 print(classes)
 
-if classes[0][0] > 0.5:
-    print("Class 1")
-else:
-    print("Class 0")
+
