72861c076a6f4b0d95d97b968c1ef6ddd8159241
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-05-20 14:26:12 +0530

    audio and feature plotter

--- plot_audio.py
+++ plot_audio.py
@@ -2,30 +2,31 @@ import numpy as np
 import librosa
 import matplotlib.pyplot as plt
 
-
 time = 2.0
 alpha = 0.97
 
 file_path = "/Users/deb/Documents/heart-data/murmur/murmur__112_1306243000964_A.wav"
 
-signal, fs = librosa.load(file_path, sr=None)
-print(fs)
+signal, fs = librosa.load(file_path, sr=16000)
 signal = signal[0: int(time * fs)]
+print(fs)
 
+# for sig in signal:
+#     print(round(sig, 4), end=", \t")
 
-emphasized_signal = np.append(signal[0], signal[1:]-alpha*signal[:-1])
+# emphasized_signal = np.append(signal[0], signal[1:]-alpha*signal[:-1])
 
-fig, ax = plt.subplots(3, 1)
-fig.tight_layout()
-ax[0].plot(signal)
-ax[0].set_ylabel("Amplitude")
-ax[0].set_xlabel("Time (millisecond)")
-ax[0].grid(True)
-
-ax[1].plot(emphasized_signal)
-ax[1].set_ylabel("Amplitude")
-ax[1].set_xlabel("Time (millisecond)")
-ax[1].grid(True)
-ax[2].set_axis_off()
-plt.savefig("signal.png", dpi=600)
-plt.show()
+# fig, ax = plt.subplots(3, 1)
+# fig.tight_layout()
+# ax[0].plot(signal)
+# ax[0].set_ylabel("Amplitude")
+# ax[0].set_xlabel("Time (millisecond)")
+# ax[0].grid(True)
+#
+# ax[1].plot(emphasized_signal)
+# ax[1].set_ylabel("Amplitude")
+# ax[1].set_xlabel("Time (millisecond)")
+# ax[1].grid(True)
+# ax[2].set_axis_off()
+# plt.savefig("signal.png", dpi=600)
+# plt.show()
