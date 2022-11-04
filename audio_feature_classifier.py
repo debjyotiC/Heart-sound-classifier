16fc7fbb8ee873fb7785d7aed4aa37bc3a82f548
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-11-04 09:11:58 +0530

    final model

--- audio_feature_classifier.py
+++ audio_feature_classifier.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-data_type = "lmfe"
+data_type = "mfe"
 data = np.load(f"data/{data_type}.npz", allow_pickle=True)
 x_data, y_data = data['out_x'], data['out_y']
 
@@ -19,69 +19,70 @@ test_ratio = 0.10
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=1 - train_ratio)
 x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio)) 
 
-print(sorted(np.argmax(y_test, axis=1)))
+# print(len(x_test))
 
-# input_length = x_train[0].shape[0]
-#
-# train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-# validation_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-# test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-#
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Reshape((150, 26), input_shape=(input_length,)),
-#
-#     tf.keras.layers.Conv1D(8, kernel_size=3, activation='relu', padding='same'),
-#     tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
-#     tf.keras.layers.Dropout(0.2),
-#     tf.keras.layers.Conv1D(16, kernel_size=3, activation='relu', padding='same'),
-#     tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
-#     tf.keras.layers.Dropout(0.2),
-#     tf.keras.layers.Flatten(),
-#
-#     # Dense layer
-#     tf.keras.layers.Dense(classes, activation='softmax', name='y_pred')
-# ])
-#
-# # model.summary()
-# model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
-#               optimizer=tf.keras.optimizers.Adam(learning_rate=0.002), metrics=['acc'])
-#
-# # this controls the batch size
-# BATCH_SIZE = 60
-# train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
-# validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
-#
-# history = model.fit(train_dataset, epochs=150, validation_data=validation_dataset)
-#
-#
-# acc = history.history['acc']
-# val_acc = history.history['val_acc']
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# print(f"Training Accuracy: {round(np.average(acc), 3)}")
-# print(f"Validation Accuracy: {round(np.average(val_acc), 3)}")
-#
-# predicted_labels = model.predict(x_test)
-# actual_labels = y_test
-#
-# np.savez(f'data/{data_type}_test.npz', out_x=predicted_labels, out_y=actual_labels)
-#
-# epochs = range(1, len(acc) + 1)
-# fig, axs = plt.subplots(2, 1)
-#
-# # plot loss
-# axs[0].plot(epochs, loss, '-', label='Training loss')
-# axs[0].plot(epochs, val_loss, 'b', label='Validation loss')
-# axs[0].set_xlabel('Epoch')
-# axs[0].set_ylabel('Loss')
-# axs[0].grid(True)
-# axs[0].legend(loc='best')
-# # plot accuracy
-# axs[1].plot(epochs, acc, '-', label='Training acc')
-# axs[1].plot(epochs, val_acc, 'b', label='Validation acc')
-# axs[1].set_xlabel('Epoch')
-# axs[1].set_ylabel('Accuracy')
-# axs[1].grid(True)
-# axs[1].legend(loc='best')
-# plt.show()
+input_length = x_train[0].shape[0]
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+validation_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Reshape((150, 26), input_shape=(input_length,)),
+
+    tf.keras.layers.Conv1D(8, kernel_size=3, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv1D(16, kernel_size=3, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+
+    # Dense layer
+    tf.keras.layers.Dense(classes, activation='softmax', name='y_pred')
+])
+
+# model.summary()
+model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.002), metrics=['acc'])
+
+# this controls the batch size
+BATCH_SIZE = 60
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
+validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
+
+history = model.fit(train_dataset, epochs=150, validation_data=validation_dataset)
+
+model.save(f"saved_model/{data_type}_paper")
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+print(f"Training Accuracy: {round(np.average(acc), 3)}")
+print(f"Validation Accuracy: {round(np.average(val_acc), 3)}")
+
+predicted_labels = model.predict(x_test)
+actual_labels = y_test
+
+np.savez(f'data/{data_type}_2_test.npz', out_x=predicted_labels, out_y=actual_labels)
+
+epochs = range(1, len(acc) + 1)
+fig, axs = plt.subplots(2, 1)
+
+# plot loss
+axs[0].plot(epochs, loss, '-', label='Training loss')
+axs[0].plot(epochs, val_loss, 'b', label='Validation loss')
+axs[0].set_xlabel('Epoch')
+axs[0].set_ylabel('Loss')
+axs[0].grid(True)
+axs[0].legend(loc='best')
+# plot accuracy
+axs[1].plot(epochs, acc, '-', label='Training acc')
+axs[1].plot(epochs, val_acc, 'b', label='Validation acc')
+axs[1].set_xlabel('Epoch')
+axs[1].set_ylabel('Accuracy')
+axs[1].grid(True)
+axs[1].legend(loc='best')
+plt.show()
